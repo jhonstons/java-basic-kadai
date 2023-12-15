0783d15842e47388dc45bee3b9f1775c187d28d7
JTS <151023119+jhonstons@users.noreply.github.com>
2023-12-15 13:30:01 +0900

    Add files via upload

---
+++ kadai_011/Prime_Chapter11.java
@@ -0,0 +1,25 @@
+package kadai.kadai_011;
+
+public class Prime_Chapter11 {
+
+public static void main(String[] args) {
+
+    int start = 2;
+    int end   = 100;
+    int count = 0;
+
+    for(int i = start; i <= end; i++) {
+    boolean isPrime = true;
+
+    for(int j = 2; j < i; j++) {
+    if(i%j == 0) {
+    isPrime = false;
+    break;
+      }
+    }
+    if(isPrime) {
+      System.out.println(i);
+      }
+    }
+  }
+}
