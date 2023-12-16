103c0e13dd0f5dced735255a82898c7543def902
JTS <151023119+jhonstons@users.noreply.github.com>
2023-12-16 21:48:22 +0900

    Update Prime_Chapter11.java

--- kadai_011/Prime_Chapter11.java
+++ kadai_011/Prime_Chapter11.java
@@ -4,22 +4,25 @@ public class Prime_Chapter11 {
 
   public static void main(String[] args) {
 
-    int start = 2;
-    int end   = 100;
-    int count = 0;
-    boolean[] array = new boolean[start];
+    int num = 101;
 
-    for(int i = start; i < end; i++) {
-    boolean isPrime = true;
+    boolean[] array = new boolean[num];
 
-    for(int j = 2; j < i; j++) {
-    if(i%j == 0) {
-    isPrime = false;
-    break;
+    for(int i = 0; i < num; i++) {
+     array[i] = true;
+    }
+
+    for(int i = 2; i < 101; i++) {
+      for(int j = 2; j < i ; j++) {
+            if(i % j == 0) {
+               array[i] = false;
+                 break;
+        }
       }
     }
-    if(isPrime) {
-      System.out.println(i);
+    for(int i = 2; i < 101; i++) {
+      if(array[i]) {
+        System.out.println(i);
       }
     }
   }
