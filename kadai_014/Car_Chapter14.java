e91084a7d95553574bbc4858d605f083ffcb58e0
JTS <151023119+jhonstons@users.noreply.github.com>
2023-12-17 22:26:15 +0900

    Update Car_Chapter14.java

--- kadai_014/Car_Chapter14.java
+++ kadai_014/Car_Chapter14.java
@@ -1,12 +1,43 @@
 package kadai.kadai_014;
 
-public class Car_Chapter14 {
-    public static void main(String[]args) {
+  public class Car_Chapter14 {
 
-    //ギア、速度データを生成
-    CarExec_Chapter14 carGear = new CarExec_Chapter14( 1, 30);
+  private int gear  = 1;
+  private int speed = 30;
 
-    carGear.gearChange(3);
+  //コンストラクタ（初期化処理）
+  public Car_Chapter14(int gear, int speed) {
+      this.gear  = gear;
+      this.speed = speed;
+  }
+
+  //【メソッド】ギアをチェンジする
+  public void gearChange(final int afterGear) {
+
+    if(afterGear == 1){
+         speed = 10;
+      }
+      else if(afterGear == 2){
+        speed = 20;
+      }
+      else if(afterGear == 3){
+        speed = 30;
+      }
+      else if(afterGear == 4){
+        speed = 40;
+      }
+      else if(afterGear == 5){
+        speed = 50;
+      }
+      else { speed = 10; }
+
+      System.out.println("ギア" + this.gear + "から" + afterGear + "に切り替えました");
+      gear = afterGear;
 
     }
-}
+  public void run() {
+    System.out.println("速度は時速" + this.speed + "km");
+    }
+  }
+
+
